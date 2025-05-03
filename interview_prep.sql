f5d4ea49b9de2d16a2ca2f21827dd63a73c29e6f
Ctej18 <charantejqv@gmail.com>
2025-05-03 15:55:56 +0530

    Custom Sorting

--- interview_prep.sql
+++ interview_prep.sql
@@ -114,3 +114,34 @@ from products*/
 select *,
        sum(cost) over (order by cost asc rows between unbounded preceding and current row) as running_sum
 from products
+
+--Custom Sort Order
+--custom sorting
+create table happiness_index 
+("rank" int,
+country varchar(55),
+Happiness_2021 varchar(55),
+Happiness_2020 varchar(55),
+Population_2022 varchar(55)
+);
+
+insert into happiness_index ("rank",country,Happiness_2021,Happiness_2020,Population_2022) values 
+(1,	'Finland',	7.842,	7.809,	5554960),
+(2,	'Denmark',	7.62,	7.646,	5834950),
+(3,	'Switzerland',	7.571,	7.56,	8773637),
+(4,	'Iceland',	7.554,	7.504,	345393),
+(5,	'Netherlands',	7.464,	7.449,	17211447),
+(6,	'Norway',	7.392,	7.488,	5511370),
+(7,	'Sweden',	7.363,	7.353,	10218971),
+(8,	'Luxembourg',	7.324,	7.238,	642371),
+(9,	'New Zealand',	7.277,	7.3,	4898203),
+(10, 'Austria',	7.268,	7.294,	9066710)
+
+select * from (select  *,
+		case when country = 'Austria' then 3
+		     when country = 'New Zealand' then 2
+			 when country = 'Sweden' then 1
+			 else 0
+		end as custom_sort_order
+from happiness_index)
+order by custom_sort_order desc,happiness_2021 desc
