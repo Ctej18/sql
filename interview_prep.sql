8a52753b33becf265fec94a08744e83f485e44a2
Ctej18 <charantejqv@gmail.com>
2025-05-03 06:59:11 +0530

    self join

--- interview_prep.sql
+++ interview_prep.sql
@@ -16,3 +16,20 @@ select  emp_id,
 		Sum(case when salary_component_type = 'hike_percent' then value end) as hike_percent
 from emp_compensation
 group by emp_id
+
+--emp salary > manager salary (self join)
+with emp_mgr as (
+select 1 as emp_id, 'Ankit' as emp_name, 10000 as salary, 4 as mgr_id
+union all select 2 as emp_id, 'Mohit' as emp_name, 15000 as salary, 5 as mgr_id
+union all select 3 as emp_id, 'Vikas' as emp_name, 10000 as salary, 4 as mgr_id
+union all select 4 as emp_id, 'Rohit' as emp_name, 5000 as salary, 2 as mgr_id
+union all select 5 as emp_id, 'Kumar' as emp_name, 12000 as salary, 6 as mgr_id
+union all select 6 as emp_id, 'Ajay' as emp_name, 12000 as salary, 2 as mgr_id
+union all select 7 as emp_id, 'Vijay' as emp_name, 90000 as salary, 2 as mgr_id
+union all select 8 as emp_id, 'John' as emp_name, 5000 as salary, 2 as mgr_id
+)
+Select e.emp_id,e.emp_name,e.salary,m.salary,m.emp_name from emp_mgr e
+inner join emp_mgr m
+on e.mgr_id = m.emp_id
+where e.salary > m.salary
+order by m.mgr_id
